c8d37a3b82e8b1cf65ff06ae42b6eaba87ff73e0
Alexander Galaydyuk <true0r@outlook.com>
2016-11-10 17:04:39 +0200

    рисунок карандашом

--- classes/WebserviceRequestCML.php
+++ classes/WebserviceRequestCML.php
@@ -108,7 +108,8 @@ class WebserviceRequestCML
     }
     public function modeImport()
     {
-        set_time_limit(0);
+        @set_time_limit(0);
+        @ini_set('max_execution_time', '0');
 
         $type = $this->param['type'];
         $methodName = 'import'.Tools::ucfirst($type);
@@ -135,13 +136,58 @@ class WebserviceRequestCML
             $this->error = "Импорт (type: $type) на данный момент не поддерживается";
         }
     }
-
+    /**
+     * @param $xml SimpleXMLElement
+     */
     public function importCatalog($xml)
     {
         // todo hook for price product, sync othercurrencyprice module
         // ?? при изменении каталога или другой сущности его GUID изменяется
+        try {
+            if (isset($xml->Классификатор) && isset($xml->Классификатор->Группы)) {
+                $this->importCategory($xml->Классификатор->Группы);
+            }
+        } catch (ImportCMLException $e) {
+            $this->error = $e->getMessage();
+        }
 
         $this->success = "Импорт номенклатуры выполнен успешно";
+    }
+
+    /**
+     * @param $groups SimpleXMLElement
+     * @param $prop array
+     */
+    public function importCategory($groups, $prop = array())
+    {
+        /** @var ImportCML $category */
+        static $category;
+
+        if (!isset($category)) {
+            $category = new CategoryImportCML();
+        }
+
+        foreach ($groups->children() as $group) {
+            $category->catchBall($group, $prop)->save();
+
+            // add child category
+            if (isset($group->Группы)) {
+                $idParent = $category->id;
+                $levelDepthParent = DB::getInstance()->getValue(
+                    (new DbQuery())
+                        ->select('level_depth')
+                        ->from(Category::$definition['table'])
+                        ->where(Category::$definition['primary'].'='.$idParent)
+                );
+                $this->importCategory(
+                    $group->Группы,
+                    array(
+                        'id_parent' => $idParent,
+                        'level_depth' => $levelDepthParent + 1,
+                    )
+                );
+            }
+        }
     }
 
     public function checkParam()
@@ -261,4 +307,203 @@ class WebserviceRequestCML
             //'Content-Type:',
         );
     }
+}
+
+// CommerceML
+abstract class ImportCML
+{
+    public static $table = WebserviceRequestCML::MODULE_NAME;
+    public static $initialized = array();
+
+    /**
+     * @param bool $cache Флаг, хранить кеш? Cache::$local[$key] хранит только 1000 елементов, стоит хранить
+     * только группы и производителя, так как товаров может быть свыше 1000, их хранение будет затирать кеш
+     */
+    public $cache = true;
+
+    public $map = array();
+    public $mapCommon = array(
+        'guid' => 'Ид',
+    );
+
+    public $id;
+    public $targetClassName;
+    /** @var  ObjectModel Содержит целевой класс PS для импорта */
+    public $hashEntityCML;
+
+    public $prop = array();
+
+    /**
+     * @param SimpleXMLElement $xml
+     * @param array $prop
+     * @return $this
+     */
+    public function catchBall($xml, $prop = array())
+    {
+        $map = array_merge($this->map, $this->mapCommon);
+        foreach ($map as $key => $val) {
+            $this->prop[$key] = isset($xml->{$val}) ? (string) $xml->{$val} : '';
+        }
+        $this->prop = array_merge($this->prop, $prop);
+        $this->hashEntityCML = md5(implode('', $this->prop));
+
+        if (!in_array($this->targetClassName, self::$initialized)) {
+            $this->initDefaultObj();
+            self::$initialized[] = $this->targetClassName;
+        }
+        return $this;
+    }
+
+    public static function getId($guid, $cache = false)
+    {
+        if ($cache && Cache::isStored($guid)) {
+            return Cache::retrieve($guid);
+        }
+
+        $id = Db::getInstance()->getValue(
+            (new DbQuery())
+                ->select('id')
+                ->from(self::$table)
+                ->where("guid = '$guid'")
+        );
+        $cache && $id && Cache::store($guid, $id);
+        return $id;
+    }
+
+    public function initDefaultObj()
+    {
+        new HackObjectModel($this->getDefaultValues(), $this->targetClassName);
+    }
+    abstract public function getDefaultValues();
+
+    public function save()
+    {
+        if ($this->id = $this->getId($this->prop['guid'], $this->cache)) {
+            if ($this->needUpd()) {
+                return $this->update() && $this->updateEntityCML();
+            } else {
+                return true;
+            }
+        } else {
+            return $this->add() && $this->addEntityCML();
+        }
+    }
+    public function add()
+    {
+        /** @var ObjectModel $targetClass */
+        $targetClass = new $this->targetClassName();
+        foreach ($this->prop as $key => $val) {
+            property_exists($targetClass, $key) && $targetClass->{$key} = $val;
+        }
+        if ($targetClass->save()) {
+            $this->id = $targetClass->id;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function update()
+    {
+    }
+
+    public function updateEntityCML()
+    {
+        return DB::getInstance()->update(
+            self::$table,
+            array('hash' => $this->hashEntityCML), "guid = '{$this->prop['guid']}'"
+        );
+    }
+    public function addEntityCML()
+    {
+        if (!isset($this->id)) {
+            throw new ImportCMLException('Id is not set for entity CML');
+        }
+        $this->cache && Cache::store($this->prop['guid'], $this->id);
+        return DB::getInstance()->insert(
+            self::$table,
+            array('id' => $this->id, 'guid' => $this->prop['guid'], 'hash' => $this->hashEntityCML),
+            false,
+            false
+        );
+    }
+
+    public function needUpd()
+    {
+        return !Db::getInstance()->getValue(
+            (new DbQuery())
+                ->select('1')
+                ->from(self::$table)
+                ->where("guid = '{$this->prop['guid']}' AND hash = '{$this->hashEntityCML}'")
+        );
+    }
+}
+
+class CategoryImportCML extends ImportCML
+{
+    public $targetClassName = 'Category';
+
+    public $map = array(
+        'name' => 'Наименование',
+        'description' => 'Описание',
+    );
+
+    public function getDefaultValues()
+    {
+        $idParent = Configuration::get('PS_HOME_CATEGORY');
+        $catParent = new Category($idParent);
+
+        return array(
+            'active' => true,
+            'id_parent' => $idParent,
+            'level_depth' => $catParent->level_depth + 1,
+        );
+    }
+}
+
+class ProductImportCML extends ImportCML
+{
+    public $targetClassName = 'Product';
+    public $cache = false;
+
+    public $map = array(
+
+    );
+
+    public function getDefaultValues()
+    {
+        // TODO: Implement getDefaultValues() method.
+    }
+}
+
+class ManufacturerImportCML extends ImportCML
+{
+    public $targetClassName = 'Manufacturer';
+
+    public $map = array(
+
+    );
+
+    public function getDefaultValues()
+    {
+        // TODO: Implement getDefaultValues() method.
+    }
+}
+
+class HackObjectModel extends ObjectModel
+{
+    public function __construct($defaultValue, $className)
+    {
+        // init self::$loaded_classes[$className]
+        new $className();
+
+        foreach ($defaultValue as $key => $value) {
+            array_key_exists($key, self::$loaded_classes[$className])
+            && self::$loaded_classes[$className][$key] = $value;
+        }
+    }
+}
+
+class ImportCMLException extends Exception
+{
+
 }
