b1e8607e40fdcda7575cde8120f64e7a3f82d693
Alexander Galaydyuk <true0r@outlook.com>
2016-11-18 14:59:09 +0200

    add обработка ЗначениеСвойства

--- classes/FeatureValueImportCML.php
+++ classes/FeatureValueImportCML.php
@@ -2,8 +2,30 @@
 
 class FeatureValueImportCML extends ImportCML
 {
+    // ! Не использовать Значение как idCMLName, так как схема XML Товар > ЗначенияСвойства > (не определяет ИдЗначения)
+    // в отличии от Свойство > Справочник > (определено ИдЗначения). Если Значение одинаковы, тогда хеш будет
+    // связкой между ЗначенияСвойства и Справочник.
     public $idEntityCMLName = 'ИдЗначения';
     public $map = array(
         'value' => 'Значение',
     );
+
+    public static function getIdFeatureValue($guid, $value)
+    {
+        $cacheId = $guid.$value;
+        if (Cache::isStored($cacheId)) {
+            return Cache::retrieve($cacheId);
+        }
+
+        if ((!$idFeature = EntityCML::getIdTarget($guid, null, true))) {
+            throw new ImportCMLException('Свойство товара не существует');
+        }
+
+        $fields = array('id_feature' => $idFeature, 'value' => $value);
+        if (!$idFeatureValue = self::catchBall('Значение', null, $fields)) {
+            throw new ImportCMLException('Значение свойства не было создано');
+        }
+        Cache::store($cacheId, $idFeatureValue);
+        return $idFeatureValue;
+    }
 }
--- classes/ProductImportCML.php
+++ classes/ProductImportCML.php
@@ -24,7 +24,7 @@ class ProductImportCML extends ImportCML
     public static function postImport()
     {
         // Обновить правила каталога для товаров у которых сменилась категори(и|я)
-        SpecificPriceRule::applyAllRules(self::$prodToUpdSpecPriceRule);
+        SpecificPriceRule::applyAllRules(array_unique(self::$prodToUpdSpecPriceRule));
     }
 
     public function save()
@@ -42,27 +42,31 @@ class ProductImportCML extends ImportCML
             return false;
         }
 
-        $cats = $this->categories;
-        $oldCats = Product::getProductCategories($this->entity->id_target);
-        $newCats = array_diff($cats, $oldCats);
-        $delCats = array_diff($oldCats, $cats);
+        /** @var Product $product */
+        $product = $this->targetClass;
+        $idProduct = $this->entity->id_target;
+        $db = Db::getInstance();
 
-        if ($delCats || $newCats) {
-            /** @var Product $product */
-            $product = $this->targetClass;
-            // Товар мог существовать и не требовал его создания или обновления
-            !$product && $product = new Product($this->entity->id_target);
 
-            // deleteCategory|updateCategories делают вызов SpecificPriceRule::applyAllRules(),
-            // что критично для импорта большого числа товаров, стоит сделать вызов applyAllRules для множества
-            self::$prodToUpdSpecPriceRule[] = $product->id;
-            if ((empty($delCats) || !$product->deleteCategories(true)) && !$product->addToCategories($cats)) {
-                throw new ImportCMLException('Не могу добавить категории (Группы) к товару');
+        if ($cats = $this->categories) {
+            $oldCats = Product::getProductCategories($idProduct);
+            $newCats = array_diff($cats, $oldCats);
+            $delCats = array_diff($oldCats, $cats);
+
+            if ($delCats || $newCats) {
+                // Товар мог существовать и не требовал его создания или обновления
+                !$product && $product = new Product($idProduct);
+
+                // deleteCategory|updateCategories делают вызов SpecificPriceRule::applyAllRules(),
+                // что критично для импорта большого числа товаров, стоит сделать вызов applyAllRules для множества
+                self::$prodToUpdSpecPriceRule[] = $product->id;
+                if ((empty($delCats) || !$product->deleteCategories(true)) && !$product->addToCategories($cats)) {
+                    throw new ImportCMLException('Не могу добавить категории (Группы) к товару');
+                }
             }
         }
 
         if (isset($this->xml->Картинка)) {
-            $idProduct = $this->entity->id_target;
             $fields = array('id_product' => $idProduct);
             $position = Image::getHighestPosition($idProduct);
             $cover = !Image::hasImages($this->idLangDefault, $idProduct);
@@ -72,6 +76,37 @@ class ProductImportCML extends ImportCML
                 $fields['cover'] = $cover;
                 $cover && $cover = false;
                 ImportCML::catchBall($img->getName(), $img, $fields);
+            }
+        }
+
+        if (Feature::isFeatureActive()) {
+            if (isset($this->xml->ЗначенияСвойств)) {
+                $idsFeatureValue = array();
+                // $feature (ЗначенияСвойства)
+                foreach ($this->xml->ЗначенияСвойств->children() as $feature) {
+                    if ($value = (string) $feature->Значение) {
+                        $idsFeatureValue[] = FeatureValueImportCML::getIdFeatureValue((string) $feature->Ид, $value);
+                    }
+                }
+                if ($idsFeatureValue) {
+                    $idsFeatureValue = array_unique($idsFeatureValue);
+
+                    $oldIdsFeatureValue = array();
+                    foreach (Product::getFeaturesStatic($idProduct) as $row) {
+                        $oldIdsFeatureValue[] = $row['id_feature_value'];
+                    }
+                    if (array_diff($idsFeatureValue, $oldIdsFeatureValue)) {
+                        $db->delete('feature_product', "id_product = $idProduct");
+                        $db->execute(
+                            "INSERT INTO "._DB_PREFIX_."feature_product (id_feature, id_product, id_feature_value)
+                            SELECT id_feature, $idProduct, id_feature_value
+                            FROM "._DB_PREFIX_."feature_value
+                            WHERE id_feature_value IN (".implode(', ', $idsFeatureValue).")"
+                        );
+
+                        self::$prodToUpdSpecPriceRule[] = $idProduct;
+                    }
+                }
             }
         }
 
@@ -99,19 +134,27 @@ class ProductImportCML extends ImportCML
         $this->categories = array();
         if (isset($this->xml->Группы)) {
             $categories = array();
-            foreach ($this->xml->Группы->children() as $idCategory) {
-                $idCategory = EntityCML::getIdTarget((string) $idCategory, null, true);
+            foreach ($this->xml->Группы->children() as $guid) {
+                $idCategory = EntityCML::getIdTarget((string) $guid, null, true);
                 if (!$idCategory) {
-                    throw new ImportCMLException('Категория (Группа) для товара не существует');
+                    throw new ImportCMLException('Категория (Группа) товара не существует');
                 }
                 $categories[] = $idCategory;
             }
             if (!empty($categories)) {
-                $this->categories = $categories;
+                $this->categories = array_unique($categories);
                 $fields['id_category_default'] = $categories[0];
             }
         }
 
         return array_merge($fields, parent::getCalcFields());
+    }
+
+    public function getDefaultFields()
+    {
+        $fields = array(
+            'id_category_default' => (int) Configuration::get('PS_HOME_CATEGORY'),
+        );
+        return array(parent::getDefaultFields(), $fields);
     }
 }
